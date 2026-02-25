3d8dc03429daa32ebeed895bdcab87a021c0c93f
swathipriya23 <swathipriyasp97@gmail.com>
2026-02-25 23:59:38 +0530

    CRUD Operation with Django Rest Framework

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ApiOverview, name='home')
+    #path('', views.ApiOverview, name='home'),
+    path('create/', views.add_items, name='add-items'),
+    path('all/', views.view_items, name='view_items'),
+    path('update/<int:pk>/', views.update_item, name='update-item'),
+    path('item/<int:pk>/delete/', views.delete_items, name='delete-items'),
+
 ]
--- api/views.py
+++ api/views.py
@@ -1,17 +1,64 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Item
 from .serializers import ItemSerializer
+from django.shortcuts import get_object_or_404
+
+@api_view(['POST'])
+def add_items(request):
+    item = ItemSerializer(data=request.data)
+
+    # validating for already existing data
+    if Item.objects.filter(**request.data).exists():
+        raise serializers.ValidationError('This data already exists')
+
+    if item.is_valid():
+        item.save()
+        return Response(item.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
 @api_view(['GET'])
-def ApiOverview(request):
-    api_urls = {
-        'all_items': '/',
-        'Search by Category': '/?category=category_name',
-        'Search by Subcategory': '/?subcategory=category_name',
-        'Add': '/create',
-        'Update': '/update/pk',
-        'Delete': '/item/pk/delete'
-    }
+def view_items(request):
+    
+    
+    # checking for the parameters from the URL
+    if request.query_params:
+        items = Item.objects.filter(**request.query_params.dict())
+    else:
+        items = Item.objects.all()
 
-    return Response(api_urls)
+    # if there is something in items else raise error
+    if items:
+        serializer = ItemSerializer(items, many=True)
+        return Response(serializer.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+@api_view(['PUT'])
+def update_item(request, pk):
+    try:
+        item = Item.objects.get(id=pk)
+    except Item.DoesNotExist:
+        return Response(
+            {"error": "Item not found"},
+            status=status.HTTP_404_NOT_FOUND
+        )
+
+    serializer = ItemSerializer(instance=item, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+def delete_items(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    item.delete()
+    return Response(
+        {"message": "Item deleted successfully"},
+        status=status.HTTP_200_OK
+    )
